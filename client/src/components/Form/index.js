0dd5e9526af0af8d7202f1ed4c83ceb98dd8aaf9
ewhite1999 <ewhite1999@gmail.com>
2022-04-01 15:04:58 +0100

    render labels dynamically

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import FormBtn from "../FormBtn";
+import FormInputs from "../FormInputs";
 
 const Form = (props) => {
-  const { text } = props;
+  const { text, inputs } = props;
   return (
     <form>
+      {inputs.map((input) => (
+        <FormInputs label={input} />
+      ))}
       <FormBtn text={text} />
     </form>
   );
