fbf259001cdf9d7d191fb09b7facf7ae3ea43ba0
ewhite1999 <ewhite1999@gmail.com>
2022-04-01 16:27:27 +0100

    hide password on input
    if the label text matches a regex for password, the input is replaced with circles

--- client/src/components/FormInputs/index.js
+++ client/src/components/FormInputs/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 const FormInputs = ({ label }) => {
+  let type = "text";
+  if (/password/i.test(label)) type = "password";
   return (
     <div className="bg-slate-50 relative rounded-xl p-5">
       <input
         className="bg-transparent relative z-10 block w-full appearance-none focus:outline-none text-xl"
-        type="text"
+        type={type}
         name={label}
         placeholder=" "
       />
