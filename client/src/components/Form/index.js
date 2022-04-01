d03e5a9637602d7815a31894f078db382d8f5060
ewhite1999 <ewhite1999@gmail.com>
2022-04-01 15:54:37 +0100

    style form

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -5,7 +5,7 @@ import FormInputs from "../FormInputs";
 const Form = (props) => {
   const { text, inputs } = props;
   return (
-    <form>
+    <form className="flex flex-col space-y-8 m-10">
       {inputs.map((input) => (
         <FormInputs key={`${input}_input`} label={input} />
       ))}
