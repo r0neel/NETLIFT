cefdbecd412354896e9471c0885f63c818c78f98
ewhite1999 <ewhite1999@gmail.com>
2022-04-01 15:53:17 +0100

    style logo

--- client/src/pages/LoginPage/index.js
+++ client/src/pages/LoginPage/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Form from "../../components/Form";
+import logo from "../../static/imgs/logo.png";
 
 const LoginPage = () => {
   const inputs = ["username", "password"];
   return (
-    <div className=" bg-nl-darkblue h-screen container">
+    <div className=" bg-nl-darkblue h-screen container pt-20 space-y-14">
+      <img
+        className=" container object-cover h-28 w-80 rounded-full mx-auto"
+        src={logo}
+      />
       <Form text="Login" inputs={inputs} />
     </div>
   );
