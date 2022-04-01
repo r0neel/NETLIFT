925b95215f54467e3dea347bd0cda35a6ebbca2b
ewhite1999 <ewhite1999@gmail.com>
2022-04-01 15:58:54 +0100

    add margin to formbtn

--- client/src/components/FormBtn/index.js
+++ client/src/components/FormBtn/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FormBtn = ({ text }) => {
   return (
-    <button className=" bg-nl-lightblue text-nl-darkblue text-2xl font-medium py-4 px-16 rounded-xl">
+    <button className=" bg-nl-lightblue text-nl-darkblue text-2xl font-medium py-4 px-16 rounded-xl mx-auto">
       {text}
     </button>
   );
