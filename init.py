9b90e419d5f8e7cc9d907660e8e78638fd85833a
r0neel <ronbhagwakar@gmail.com>
2022-04-01 14:35:05 +0100

    Move: init.py into server directory

--- init.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-
-db = client['netLift']
-users = db['users']
-workouts = db['workouts']
-lifts = db['lifts']
-exercises = db['exercises']
