9b8c82474f213446c14134359a12f5c55ffddb3c
ewhite1999 <ewhite1999@gmail.com>
2022-04-01 14:51:49 +0100

    style formbtn

--- client/src/components/FormBtn/index.js
+++ client/src/components/FormBtn/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const FormBtn = ({ text }) => {
-  return <button>{text}</button>;
+  return (
+    <button className=" bg-nl-lightblue text-nl-darkblue text-2xl font-medium py-4 px-16 rounded-xl">
+      {text}
+    </button>
+  );
 };
 
 export default FormBtn;
