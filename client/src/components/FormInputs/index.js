df167d5683b84b94c6fae48a040d05c0b44f3b7a
ewhite1999 <ewhite1999@gmail.com>
2022-04-01 15:46:41 +0100

    floating labels

--- client/src/components/FormInputs/index.js
+++ client/src/components/FormInputs/index.js
@@ -1,10 +1,19 @@
 import React from "react";
-
 const FormInputs = ({ label }) => {
   return (
-    <div>
-      <input type="text" name={label} placeholder=" " />
-      <label htmlFor={label}>{label}</label>
+    <div className="bg-slate-50 relative rounded-xl p-5">
+      <input
+        className="bg-transparent z-10 block w-full appearance-none focus:outline-none text-xl"
+        type="text"
+        name={label}
+        placeholder=" "
+      />
+      <label
+        className=" origin-0 absolute top-5 text-nl-darkblue text-xl"
+        htmlFor={label}
+      >
+        {label}:
+      </label>
     </div>
   );
 };
