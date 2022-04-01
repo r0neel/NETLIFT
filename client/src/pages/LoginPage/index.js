40a604e3612c192f894ec9e678dc44b9e081564a
ewhite1999 <ewhite1999@gmail.com>
2022-04-01 16:02:01 +0100

    change spacing on LoginPage

--- client/src/pages/LoginPage/index.js
+++ client/src/pages/LoginPage/index.js
@@ -5,7 +5,7 @@ import logo from "../../static/imgs/logo.png";
 const LoginPage = () => {
   const inputs = ["username", "password"];
   return (
-    <div className=" bg-nl-darkblue h-screen container pt-20 space-y-14">
+    <div className=" bg-nl-darkblue h-screen container pt-16 space-y-20">
       <img
         className=" container object-cover h-28 w-80 rounded-full mx-auto"
         src={logo}
