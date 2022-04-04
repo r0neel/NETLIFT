3ce6076ed5d2d654985c9403a6a16e063aea6586
ewhite1999 <ewhite1999@gmail.com>
2022-04-04 12:00:15 +0100

    style workout preview

--- client/src/components/WorkoutPreview/index.js
+++ client/src/components/WorkoutPreview/index.js
@@ -4,22 +4,26 @@ import PropTypes from "prop-types";
 const WorkoutPreview = (props) => {
   const { day, exercises } = props;
 
+  // TODO: change the weight to what they are actually doing
   return (
-    <>
-      <p>{day}</p>
-      <ul>
+    <div className=" bg-nl-lightblue text-nl-darkblue rounded-xl py-2 px-6">
+      <p className=" text-nl-textgrey text-center mb-3">{day}</p>
+      <ul className=" space-y-1">
         {exercises.map((exercise) => {
           return (
-            <li key={exercise.name}>
+            <li
+              key={exercise.name}
+              className=" font-medium flex justify-between"
+            >
               <span>{exercise.name}</span>
               <span>
-                {exercise.sets} x {exercise.reps}
+                {exercise.sets} x {exercise.reps} 100kg
               </span>
             </li>
           );
         })}
       </ul>
-    </>
+    </div>
   );
 };
 
