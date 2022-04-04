ca9c799b017701fb6c0194d0000de66b7ac49a52
capriceebanks <capri.ee@outlook.com>
2022-04-04 11:10:49 +0100

    added history, login and progress pages

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,2 +1,6 @@
 export { default as LoginPage } from "./LoginPage";
 export { default as RegisterPage } from "./RegisterPage";
+export { default as History } from './History'
+export { default as Progress } from './Progress'
+export { default as Settings } from './Settings'
+
