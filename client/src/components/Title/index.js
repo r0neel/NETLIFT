83ae369a45cc3323d17cc36dd9545a3051614153
ewhite1999 <ewhite1999@gmail.com>
2022-04-04 12:08:19 +0100

    add more margin on title component

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 const Title = ({ text }) => {
   return (
-    <div className="flex justify-between items-center py-6">
+    <div className="flex justify-between items-center pt-6 pb-10">
       {/* TODO: Add cropped logo */}
       <img className="object-cover w-20 h-12 rounded-full" src={logo} />
       {/* TODO: make this a link to home */}
