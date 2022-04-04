5d648b6ffdc734ff6ee3f1b2734d709dd5837263
ewhite1999 <ewhite1999@gmail.com>
2022-04-04 12:08:01 +0100

    add workout num to workout preview

--- client/src/components/WorkoutPreview/index.js
+++ client/src/components/WorkoutPreview/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const WorkoutPreview = (props) => {
-  const { day, exercises } = props;
+  const { day, exercises, workoutNum } = props;
 
   // TODO: change the weight to what they are actually doing
   return (
     <div className=" bg-nl-lightblue text-nl-darkblue rounded-xl py-2 px-6">
-      <p className=" text-nl-textgrey text-center mb-3">{day}</p>
+      <div className="flex justify-between text-nl-textgrey mb-2">
+        <span>Workout {workoutNum} </span>
+        <span>{day}</span>
+      </div>
       <ul className=" space-y-1">
         {exercises.map((exercise) => {
           return (
@@ -30,6 +33,7 @@ const WorkoutPreview = (props) => {
 WorkoutPreview.propTypes = {
   day: PropTypes.string.isRequired,
   exercises: PropTypes.array.isRequired,
+  workoutNum: PropTypes.number.isRequired,
 };
 
 export default WorkoutPreview;
