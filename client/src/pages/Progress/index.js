871d3c34d420bcf58890ce1ba43d3c428acb24d4
capriceebanks <capri.ee@outlook.com>
2022-04-04 17:34:35 +0100

    added basic styling to progress page

--- client/src/pages/Progress/index.js
+++ client/src/pages/Progress/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Progress = () => {
-return(
-<>
-<h1 className='pt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, in nesciunt. Doloribus, fugiat earum. Perspiciatis necessitatibus facere corporis eligendi veritatis. Architecto, nisi nostrum quas alias illum iusto aliquid eos praesentium!</h1>
-lorem
-
-</>
-)};
+  return(
+    <div className="bg-nl-darkblue min-h-screen px-8">
+      
+    </div>
+  );};
 export default Progress;
