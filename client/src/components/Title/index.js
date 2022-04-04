6942c641c11a32137e712dbcc78c9a2ebe3bf207
ewhite1999 <ewhite1999@gmail.com>
2022-04-04 11:59:52 +0100

    create title component

---
+++ client/src/components/Title/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "../../static/imgs/logo.png";
+import PropTypes from "prop-types";
+
+const Title = ({ text }) => {
+  return (
+    <div className="flex justify-between items-center py-6">
+      {/* TODO: Add cropped logo */}
+      <img className="object-cover w-20 h-12 rounded-full" src={logo} />
+      {/* TODO: make this a link to home */}
+      <h1 className=" text-slate-50 text-3xl font-bold">{text}</h1>
+    </div>
+  );
+};
+
+Title.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Title;
