82864ef5541f939566b41f18e8cb36fae07c5763
ewhite1999 <ewhite1999@gmail.com>
2022-04-05 16:11:55 +0100

    handle when sets overflow screen

--- client/src/components/Exercise/index.js
+++ client/src/components/Exercise/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import SetBtn from "../SetBtn";
 
 const Exercise = ({ name, reps, sets }) => {
-  return <></>;
+  const setsArr = [];
+  for (let i = 0; i < 10; i++) {
+    setsArr.push(<SetBtn reps={reps} />);
+  }
+  return (
+    <div className=" overflow-x-scroll overflow-y-hidden">
+      <h3>{name}</h3>
+      <div className="flex justify-between space-x-4 whitespace-nowrap min-w-fit">
+        {setsArr}
+      </div>
+    </div>
+  );
 };
 
 Exercise.propTypes = {
