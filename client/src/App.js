5f69c8c78850f9dbd6e03855abbc6f9c7f65b238
capriceebanks <capri.ee@outlook.com>
2022-04-05 15:11:41 +0100

    added create page to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 
 import {
-  History,
   LoginPage,
-  Progress,
   RegisterPage,
-  Settings,
+  Create,
   ProgramsPage,
+  History,
+  Progress,
+  Settings,
 } from "./pages";
 
 import { NavBar } from "./components";
@@ -20,6 +21,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/create" element={<Create />} />
         <Route path="/home" element={<ProgramsPage />} />
         <Route path="/history" element={<History />} />
         <Route path="/progress" element={<Progress />} />
