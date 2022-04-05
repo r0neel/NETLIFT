5c38f5347aba694b33f9670e435e6e92cfb9f0d7
capriceebanks <capri.ee@outlook.com>
2022-04-05 09:50:56 +0100

    formatted documents

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import {  Route, Routes, useLocation } from 'react-router-dom';
+import React from "react";
+import {  Route, Routes, useLocation } from "react-router-dom";
 
 
 import { History, LoginPage, Progress, RegisterPage, Settings, ProgramsPage } from "./pages";
-import { NavBar } from './components'
+import { NavBar } from "./components";
 
 const App = () => {
   
-  let location = useLocation()
+  let location = useLocation();
   
-  // let showNav = 
   return(
-  <>
-    <Routes>
-      {/* <Route path='/home' element={<Home />}/> */}
-        <Route path='/login' element={ <LoginPage /> }/>
+    <>
+      <Routes>
+        <Route path='login' element={ <LoginPage /> }/>
         <Route path='register' element={ <RegisterPage /> }/>
         <Route path='home' element={ <ProgramsPage/> }/>
         <Route path='history' element={ <History /> }/>
         <Route path='progress' element={ <Progress /> }/>
         <Route path='settings' element={ <Settings />}/>
-    </Routes>
-    {location.pathname === '/login' || location.pathname === '/register' ? null : <NavBar />}
-  </>
-)};
+      </Routes>
+      {location.pathname === "/login" || location.pathname === "/register" ? null : <NavBar />}
+    </>
+  );};
 
 export default App;
