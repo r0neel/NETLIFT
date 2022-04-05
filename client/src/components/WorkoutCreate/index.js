755fc8e0265d94c980949a79a1e4e04e183a735a
capriceebanks <capri.ee@outlook.com>
2022-04-05 15:39:43 +0100

    added workoutcreate component

---
+++ client/src/components/WorkoutCreate/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// TODO: Animate pages so it falls smoothly on page 
+const WorkoutCreate = () => {
+  return(
+    <>
+
+    </>
+  );};
+export default WorkoutCreate;
