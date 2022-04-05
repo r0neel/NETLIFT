365e9fb547f9f2d7c98fe93dbad9107c7059f10b
capriceebanks <capri.ee@outlook.com>
2022-04-05 15:41:03 +0100

    exported workoutcreate

--- client/src/components/WorkoutCreate/index.js
+++ client/src/components/WorkoutCreate/index.js
@@ -4,7 +4,7 @@ import React from "react";
 const WorkoutCreate = () => {
   return(
     <>
-
+    
     </>
   );};
 export default WorkoutCreate;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as NavBar } from "./NavBar";
 export { default as Title } from "./Title";
+export { default as WorkoutCreate } from "./WorkoutCreate";
 
