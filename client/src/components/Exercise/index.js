0e640bf7932205482b799108859636e5bd3efd3b
ewhite1999 <ewhite1999@gmail.com>
2022-04-05 16:15:48 +0100

    style componets

--- client/src/components/Exercise/index.js
+++ client/src/components/Exercise/index.js
@@ -4,12 +4,14 @@ import SetBtn from "../SetBtn";
 
 const Exercise = ({ name, reps, sets }) => {
   const setsArr = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < sets; i++) {
     setsArr.push(<SetBtn reps={reps} />);
   }
   return (
-    <div className=" overflow-x-scroll overflow-y-hidden">
-      <h3>{name}</h3>
+    <div className=" overflow-x-scroll overflow-y-hidden space-y-3">
+      <h3 className=" text-2xl text-nl-darkblue capitalize font-medium">
+        {name}
+      </h3>
       <div className="flex justify-between space-x-4 whitespace-nowrap min-w-fit">
         {setsArr}
       </div>
