2e05672dade21f140e02881bec5f5bddd4cbbc61
ewhite1999 <ewhite1999@gmail.com>
2022-04-05 16:23:16 +0100

    add workout section

---
+++ client/src/components/WorkoutSection/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Exercise from "../Exercise";
+const WorkoutSection = () => {
+  return (
+    <>
+      <Exercise sets={5} reps={5} name={"squat"} />
+    </>
+  );
+};
+
+export default WorkoutSection;
