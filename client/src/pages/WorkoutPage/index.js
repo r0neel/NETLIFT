335b362e9f6eab6dd7e3c0567e7732c635034b11
ewhite1999 <ewhite1999@gmail.com>
2022-04-05 16:50:31 +0100

    load workout section with fake data

--- client/src/pages/WorkoutPage/index.js
+++ client/src/pages/WorkoutPage/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Title from "../../components/Title";
+import WorkoutSection from "../../components/WorkoutSection";
 
 const WorkoutPage = () => {
-  return <></>;
+  const workouts = [
+    { name: "Squats", reps: 5, sets: 5 },
+    { name: "OHP", reps: 5, sets: 12 },
+    { name: "Deadlift", reps: 5, sets: 1 },
+  ];
+  return (
+    <div className="bg-nl-darkblue min-h-screen px-8">
+      <Title text="Workout" />
+      <div className="-mx-8 px-8 bg-nl-lightgrey space-y-8">
+        <WorkoutSection workouts={workouts} />
+      </div>
+    </div>
+  );
 };
 
 export default WorkoutPage;
