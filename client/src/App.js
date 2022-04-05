dcdf1e014d156db25bf91fcca51ade888bf48ec6
capriceebanks <capri.ee@outlook.com>
2022-04-05 12:12:35 +0100

    title bug fixed; no nested routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,35 @@
 import React from "react";
-import {  Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
-
-import { History, LoginPage, Progress, RegisterPage, Settings, ProgramsPage } from "./pages";
-import { NavBar, Title } from "./components";
+import {
+  History,
+  LoginPage,
+  Progress,
+  RegisterPage,
+  Settings,
+  ProgramsPage,
+} from "./pages";
+import { NavBar } from "./components";
 
 const App = () => {
-  
   let location = useLocation();
 
-  return(
+  return (
     <>
-      {/* {location.pathname === "/login" || location.pathname === "/register" ? null : <Title text={location.key} />} */}
       <Routes>
-        <Route path='login' element={ <LoginPage /> }/>
-        <Route path='register' element={ <RegisterPage /> }/>
-        <Route path='home' id="Home" element={ <ProgramsPage/> }/>
-        <Route path='history' element={ <History /> }/>
-        <Route path='progress' element={ <Progress /> }/>
-        <Route path='settings' element={ <Settings />}/>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="home" element={<ProgramsPage />} />
+        <Route path="history" element={<History />} />
+        <Route path="progress" element={<Progress />} />
+        <Route path="settings" element={<Settings />} />
       </Routes>
-      {location.pathname === "/login" || location.pathname === "/register" ? null : <NavBar />}
+      {location.pathname === "/login" ||
+			location.pathname === "/register" ? null : (
+          <NavBar />
+        )}
     </>
-  );};
+  );
+};
 
 export default App;
--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -6,10 +6,10 @@ import PropTypes from "prop-types";
 const Title = ({ text }) => {
   return (
     <div className="flex justify-between items-center pt-6 pb-10">
-      <Link to="home">
+      <Link to="/home">
         <img className="object-cover w-20 h-12 rounded-full" src={logo} />
       </Link>
-      <Link to="home">
+      <Link to="/home">
         <h1 className=" text-slate-50 text-3xl font-bold">{text}</h1>
       </Link>
     </div>
