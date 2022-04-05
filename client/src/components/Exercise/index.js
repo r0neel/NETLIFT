465e7e18ec0a2109c2ee7c52629bf5720ab42c86
ewhite1999 <ewhite1999@gmail.com>
2022-04-05 15:44:15 +0100

    create Exercise component

---
+++ client/src/components/Exercise/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Exercise = ({ name, reps, sets }) => {
+  return <></>;
+};
+
+Exercise.propTypes = {
+  name: PropTypes.string.isRequired,
+  reps: PropTypes.number.isRequired,
+  sets: PropTypes.number.isRequired,
+};
+
+export default Exercise;
