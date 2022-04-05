50fab0dda073042d8a290267315d93766744f952
ewhite1999 <ewhite1999@gmail.com>
2022-04-05 15:34:35 +0100

    add logic to setbtn

--- client/src/components/SetBtn/index.js
+++ client/src/components/SetBtn/index.js
@@ -2,9 +2,20 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const SetBtn = ({ reps }) => {
-  const [repsDone, setRepsDone] = useState({ reps });
+  const [repsDone, setRepsDone] = useState(reps);
   const [finished, setFinished] = useState(false);
-  return <button>{repsDone}</button>;
+  const reset = () => {
+    setRepsDone(reps);
+    setFinished(false);
+  };
+  const handleReps = () => {
+    repsDone > 1 ? setRepsDone(repsDone - 1) : reset();
+  };
+  const clickHandler = () => {
+    finished ? handleReps() : setFinished(true);
+  };
+
+  return <button onClick={clickHandler}>{repsDone}</button>;
 };
 
 SetBtn.propTypes = {
