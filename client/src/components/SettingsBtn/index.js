ff8a934945f8168ef2108c7b01e4c0b1d681fe25
ewhite1999 <ewhite1999@gmail.com>
2022-04-06 10:00:40 +0100

    add settings btn component

---
+++ client/src/components/SettingsBtn/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const SettingsBtn = ({ text, to }) => {
+  return (
+    <Link
+      to={`/settings/${to}`}
+      className=" bg-nl-lightblue text-nl-darkblue text-2xl font-medium py-4 px-16 rounded-xl mx-auto"
+    >
+      {text}
+    </Link>
+  );
+};
+
+SettingsBtn.propTypes = {
+  text: PropTypes.string.isRequired,
+  to: PropTypes.text.isRequired,
+};
+
+export default SettingsBtn;
