b6225a712e4f55945ea104b217733a0d9765013c
ewhite1999 <ewhite1999@gmail.com>
2022-04-06 10:18:39 +0100

    set up router for settings page

--- client/src/pages/Settings/index.js
+++ client/src/pages/Settings/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 import { Title } from "../../components";
+import SettingsHome from "./SettingsHome";
 
 const Settings = () => {
   return (
     <div className="bg-nl-darkblue min-h-[calc(100vh-73px)] px-8">
       <Title text="Settings" />
+      <Routes>
+        <Route path="/" element={<SettingsHome />} />
+      </Routes>
     </div>
   );
 };
