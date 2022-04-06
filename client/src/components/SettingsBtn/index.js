51e3956b133f94c33843648e3429b305d198b3bc
ewhite1999 <ewhite1999@gmail.com>
2022-04-06 10:13:17 +0100

    add hover effect to form and settings btns

--- client/src/components/SettingsBtn/index.js
+++ client/src/components/SettingsBtn/index.js
@@ -6,7 +6,7 @@ const SettingsBtn = ({ text, to }) => {
   return (
     <Link
       to={`/settings/${to}`}
-      className=" bg-nl-lightblue text-nl-darkblue text-2xl font-medium py-4 px-16 rounded-xl mx-auto"
+      className=" bg-nl-lightblue text-nl-darkblue text-2xl font-medium py-4 px-16 rounded-xl mx-auto hover:opacity-80"
     >
       {text}
     </Link>
