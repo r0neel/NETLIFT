5b1f21299259689d9d80e86b44ab26d04f1d3165
r0neel <ronbhagwakar@gmail.com>
2022-04-06 12:53:05 +0100

    Add: fetch profile action

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,10 +1,4 @@
-// export default addUser = e => ({
-//   type: "REGISTER",
-//   payload: {username:e.target.username, email:e.target.email, password:e.target.password}
-// })
-
 import axios from "axios";
-
 
 export const addUser = (e) => {
   return async (dispatch) => {
@@ -14,14 +8,33 @@ export const addUser = (e) => {
         username: e.target.username.value,
         password: e.target.password.value
       });
-      let token = data.access_token;
+      let token = `Bearer ${data.access_token}`;
+      localStorage.setItem("token", token);
       dispatch({
         type: "LOGIN",
-        payload: {user: username,
-          token : token
-        }
+        payload: { user: username, token: token }
       });
-      
+    } catch (err) {
+      dispatch({
+        type: "SET_ERROR",
+        payload: err
+      });
+    }
+  };
+};
+
+export const fetchProfile = () => {
+  return async (dispatch) => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const { data } = await axios.get("http://127.0.0.1:5000/user", {
+        headers: { Authorization: token }
+      });
+      dispatch({
+        type: "LOAD_USER",
+        payload: data
+      });
     } catch (err) {
       dispatch({
         type: "SET_ERROR",
