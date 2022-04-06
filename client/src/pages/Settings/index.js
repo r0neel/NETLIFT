524f92360f32408350a263d6cbc6c39f9a3de5f1
ewhite1999 <ewhite1999@gmail.com>
2022-04-06 11:11:25 +0100

    conditionally render a back btn on the settings pages
    if the page route is anything other than the index route there will be a button to take you back to the index route

--- client/src/pages/Settings/index.js
+++ client/src/pages/Settings/index.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import { Title } from "../../components";
+import BackSettings from "../../components/BackSettings";
 import SettingsHome from "./SettingsHome";
+import SettingsUser from "./SettingsUsername";
 
 const Settings = () => {
+  let { pathname } = useLocation();
+  console.log(pathname);
   return (
     <div className="bg-nl-darkblue min-h-[calc(100vh-73px)] px-8 flex flex-col">
       <Title text="Settings" />
+      {pathname === "/settings" ? null : <BackSettings />}
       <Routes>
         <Route path="/" element={<SettingsHome />} />
+        <Route path="/username" element={<SettingsUser />} />
       </Routes>
     </div>
   );
