963b5256638456dd22ed7afbe68c7242d8ef48a3
ewhite1999 <ewhite1999@gmail.com>
2022-04-07 14:02:30 +0100

    create Select Equipment component

---
+++ client/src/components/CreateProgramComponents/SelectEquipmemt/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SelectEquipment = () => {
+  const equipment = [
+    "barbell",
+    "body weight",
+    "cable",
+    "dumbbell",
+    "kettlebell",
+    "tire",
+    "trap bar",
+  ];
+  return (
+    <>
+      <label htmlFor="equipment">Select the equipment you want to use</label>
+      <select name="equipment" id="equipment">
+        {equipment.sort().map((part) => (
+          <option key={`equipment_${part}`} value={part}>
+            {part}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
+
+export default SelectEquipment;
