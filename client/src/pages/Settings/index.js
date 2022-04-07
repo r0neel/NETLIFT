6b78d81f24e592f13bdfcc153654f48057b37067
capriceebanks <capri.ee@outlook.com>
2022-04-07 20:21:35 +0100

    added log out route to settings

--- client/src/pages/Settings/index.js
+++ client/src/pages/Settings/index.js
@@ -5,6 +5,7 @@ import { Title, LogOut } from "../../components";
 import BackSettings from "../../components/BackSettings";
 import SettingsDelete from "./SettingsDelete";
 import SettingsHome from "./SettingsHome";
+import SettingsLogout from "./SettingsLogout";
 import SettingsPassword from "./SettingsPassword";
 import SettingsUnit from "./SettingsUnit";
 import SettingsUser from "./SettingsUsername";
@@ -28,6 +29,7 @@ const Settings = () => {
         <Route path="/username" element={<SettingsUser />} />
         <Route path="/password" element={<SettingsPassword />} />
         <Route path="/unit" element={<SettingsUnit />} />
+        <Route path="/log-out" element={<SettingsLogout />} />
         <Route path="/delete-account" element={<SettingsDelete />} />
       </Routes>
     </div>
