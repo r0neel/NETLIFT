a7e37709cafa98a7b62d7e5090c4626bea86ba08
capriceebanks <capri.ee@outlook.com>
2022-04-07 12:30:36 +0100

    edited err page

--- client/src/pages/Err404/index.js
+++ client/src/pages/Err404/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Err404 = () => {
 return(
-<div className='text-center'>
- <h1 className=' text-slate-50 text-3xl font-bold'>ERROR</h1>
- <h1 className=' text-slate-50 text-xl font-bold'>Seems like this page doesn't exist...</h1>
+<div className='bg-nl-darkblue'>
+  <div className='text-center '>
+    <h1 className='mt-10 text-slate-50 text-3xl font-bold'>ERROR</h1>
+    <h1 className='text-slate-50 text-xl font-bold'>Seems like this page doesn't exist...</h1>
+  </div>
 </div>
 )};
 export default Err404;
