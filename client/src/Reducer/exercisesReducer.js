5089db7ee59ee0ef6a41ec9e56aa5c5319f25d25
Dafe-akaka <dafe.akaka123@gmail.com>
2022-04-07 14:21:03 +0100

    setup: exercise reducer

---
+++ client/src/Reducer/exercisesReducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+    exercise: []
+};
+
+
+const exercisesReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "LOAD_EXERCISES": {
+          const exercises = action.payload;
+          return { ...state, exercise: exercises };
+        }
+        default:
+          return state;
+      }
+}
+
+
+export default exercisesReducer
