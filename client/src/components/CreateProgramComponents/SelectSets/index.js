df4d815c58f67176ecbb71818abcbc98bbf3b28e
ewhite1999 <ewhite1999@gmail.com>
2022-04-07 14:02:43 +0100

    create select Sets component

---
+++ client/src/components/CreateProgramComponents/SelectSets/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SelectSets = ({ name, max }) => {
+  return (
+    <>
+      <label htmlFor={name}>How many {name} would you like to do?</label>
+      <input name={name} id={name} type="number" min={1} max={max} />
+    </>
+  );
+};
+
+export default SelectSets;
