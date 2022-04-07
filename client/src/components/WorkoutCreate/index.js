414ff8ec2db90b7e8566fdf94b3cc4c5722c62c6
capriceebanks <capri.ee@outlook.com>
2022-04-07 20:59:43 +0100

    added exercise link button

--- client/src/components/WorkoutCreate/index.js
+++ client/src/components/WorkoutCreate/index.js
@@ -1,31 +1,31 @@
 import React from "react";
 // import axios from "axios";
-
-import CreateForm from "../CreateForm";
-
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // TODO: Animate pages so it falls smoothly on page 
+
+// TODO: Make an add exercise button that links to /test route
+// TODO: Get results from usestate/effect to display what exercises were selected
+// TODO: style /test page
+// TODO: push results onto programs page
+
 const WorkoutCreate = () => {
-  const inputs = [
-    { 
-      text: "title",
-      message: "Max thirty characters",
-      pattern: ".{0,30}"
-    }, 
-    { 
-      text: "weight",
-      message: "Starting weight"
-    },
-    { 
-      text: "exercise",
-      message: "Starting weight"
-    },
-  ];
+  
 
   return(
     <div className="mx-auto min-h-screen container pt-16">
-      <CreateForm text="Create" inputs={inputs}>
-      
-      </CreateForm>
+      <form
+      role="form"
+      className="flex flex-col space-y-8"
+    >
+      <div className="">
+        <FontAwesomeIcon className="w-8 h-8" icon={faPlus}></FontAwesomeIcon>
+      </div>
+
+      <button className=" bg-nl-lightblue text-nl-darkblue text-2xl font-medium py-4 px-16 rounded-xl mx-auto hover:opacity-80">
+      Create
+      </button>
+      </form>
     </div>
   );};
 export default WorkoutCreate;
