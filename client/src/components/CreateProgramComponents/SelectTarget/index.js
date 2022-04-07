46bd8d1b8d5f2f19b2032fb8a647ff539b2b8590
ewhite1999 <ewhite1999@gmail.com>
2022-04-07 14:02:54 +0100

    add select target component

---
+++ client/src/components/CreateProgramComponents/SelectTarget/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const SelectTarget = () => {
+  const bodyParts = [
+    "back",
+    "cardio",
+    "chest",
+    "lower arms",
+    "lower legs",
+    "neck",
+    "shoulders",
+    "upper arms",
+    "upper legs",
+    "waist",
+  ];
+  return (
+    <>
+      <label htmlFor="target">Select the body part you want to target</label>
+      <select name="target" id="target">
+        {bodyParts.sort().map((part) => (
+          <option key={`bodyPart_${part}`} value={part}>
+            {part}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
+
+export default SelectTarget;
