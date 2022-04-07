06a19cc1bb4003a9ec996c38dd779357598ef1e1
ewhite1999 <ewhite1999@gmail.com>
2022-04-07 15:01:38 +0100

    load exercises from server

--- client/src/components/CreateProgramComponents/CreateExercise/index.js
+++ client/src/components/CreateProgramComponents/CreateExercise/index.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import SelectEquipment from "../SelectEquipmemt";
 import SelectSets from "../SelectSets";
 import SelectTarget from "../SelectTarget";
+import SelectExercise from "../SelectExercise";
 
 // TODO: Animate pages so it falls smoothly on page
 const WorkoutCreate = () => {
-  const loadExercises = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    const target = form.target.value;
-    const equipment = form.equipment.value;
-    const sets = form.sets.value;
-    const reps = form.reps.value;
-    console.log(target, equipment, sets, reps);
+  const [exercises, setExercises] = useState();
+  const [reps, setReps] = useState();
+  const [sets, setSets] = useState();
+
+  const reset = () => {
+    setExercises(null);
   };
+  const loadExercises = async (e) => {
+    e.preventDefault();
+    try {
+      const form = e.currentTarget;
+      const target = form.target.value;
+      const equipment = form.equipment.value;
+      const setsVal = form.sets.value;
+      setSets(setsVal);
+      const repsVal = form.reps.value;
+      setReps(repsVal);
+      const queryString = `${process.env.API_URL}exercises?bodyPart=${target}&equipment=${equipment}`;
+      const { data } = await axios.get(queryString);
+      setExercises(data);
+    } catch (e) {
+      alert("We don't have any exercises matching that criteria");
+    }
+  };
+
   return (
     <div className="mx-auto min-h-screen container pt-16">
-      <form onSubmit={loadExercises}>
+      <form onSubmit={loadExercises} onChange={reset}>
         <SelectTarget />
         <SelectEquipment />
         <SelectSets name={"sets"} max={20} />
         <SelectSets name={"reps"} />
         <input type="submit" value="Find exercises" />
       </form>
+      {exercises && <SelectExercise data={exercises} />}
     </div>
   );
 };
