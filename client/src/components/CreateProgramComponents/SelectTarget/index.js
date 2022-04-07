aecab1f2e95226d99eed0818deea6162dc18c11a
ewhite1999 <ewhite1999@gmail.com>
2022-04-07 16:06:11 +0100

    style select target

--- client/src/components/CreateProgramComponents/SelectTarget/index.js
+++ client/src/components/CreateProgramComponents/SelectTarget/index.js
@@ -15,8 +15,13 @@ const SelectTarget = () => {
   ];
   return (
     <>
-      <label htmlFor="target">Select the body part you want to target</label>
-      <select required name="target" id="target">
+      <label htmlFor="target">Select the body part you want to target:</label>
+      <select
+        className="mt-3 bg-nl-darkblue text-slate-50 text-xl py-2 px-4 rounded-xl"
+        required
+        name="target"
+        id="target"
+      >
         {bodyParts.sort().map((part) => (
           <option key={`bodyPart_${part}`} value={part}>
             {part}
