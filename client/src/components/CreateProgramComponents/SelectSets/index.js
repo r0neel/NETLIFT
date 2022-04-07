9d256000722af724ba668369e014c730683d3855
ewhite1999 <ewhite1999@gmail.com>
2022-04-07 16:09:56 +0100

    style sets

--- client/src/components/CreateProgramComponents/SelectSets/index.js
+++ client/src/components/CreateProgramComponents/SelectSets/index.js
@@ -4,7 +4,15 @@ const SelectSets = ({ name, max }) => {
   return (
     <>
       <label htmlFor={name}>How many {name} would you like to do?</label>
-      <input required name={name} id={name} type="number" min={1} max={max} />
+      <input
+        className="mt-3 bg-nl-darkblue text-slate-50 text-xl py-2 px-4 rounded-xl"
+        required
+        name={name}
+        id={name}
+        type="number"
+        min={1}
+        max={max}
+      />
     </>
   );
 };
